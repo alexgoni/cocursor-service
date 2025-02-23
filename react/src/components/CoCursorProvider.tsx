3da1a6b78872e9347efa9083bca8198901421d98
alexgoni <goni000211@gmail.com>
2025-02-23 14:40:40 +0900

    feat: 훅 추가

--- react/src/components/CoCursorProvider.tsx
+++ react/src/components/CoCursorProvider.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
 import Cursor from "./Cursor";
 import throttle from "../utils/throttle";
+import { CoCursorContext } from "./CoCursorContext";
 
 export interface CursorData {
   id: string;
@@ -14,50 +15,36 @@ interface Props {
   children: ReactNode;
   channel?: string;
   myName?: string;
-  blockInfoSend?: boolean;
+  allowInfoSend?: boolean;
   quality?: "high" | "middle" | "low";
+  disabled?: boolean;
 }
 
 export default function CoCursorProvider({
   children,
-  channel,
-  myName,
-  blockInfoSend = false,
-  quality = "high",
+  channel: initialChannel,
+  myName: initialMyName,
+  allowInfoSend: initialAllowInfoSend = true,
+  quality: initialQuality = "high",
+  disabled: initialDisabled = false,
 }: Props) {
+  const [channel, setChannel] = useState(initialChannel);
+  const [myName, setMyName] = useState(initialMyName);
+  const [allowInfoSend, setAllowInfoSend] = useState(initialAllowInfoSend);
+  const [quality, setQuality] = useState<"high" | "middle" | "low">(
+    initialQuality
+  );
   const [cursors, setCursors] = useState<Record<string, CursorData>>({});
+  const [disabled, setDisabled] = useState(initialDisabled);
   const ws = useRef<WebSocket | null>(null);
   const userId = useRef(`user-${Math.random().toString(36).substring(2, 11)}`);
-  const userName = useRef(myName || `anonymous`);
-
-  // 웹소켓 연결 및 설정
-  useEffect(() => {
-    let wsURL = `ws://localhost:8080`;
-    if (channel) {
-      wsURL = `ws://localhost:8080?channel=${channel}`;
-    }
-
-    // 웹소켓 연결
-    ws.current = new WebSocket(wsURL, [import.meta.env.VITE_COCURSOR_API_KEY]);
-
-    // 웹소켓 메시지 수신
-    ws.current.onmessage = (event) => {
-      const cursorData: CursorData = JSON.parse(event.data);
-      setCursors((prev) => ({ ...prev, [cursorData.id]: cursorData }));
-    };
-
-    return () => {
-      // 웹소켓 연결 종료
-      ws.current?.close();
-    };
-  }, [channel]);
 
   const sendCursorPosition = useCallback(
     (e: MouseEvent) => {
       if (
         !ws.current ||
         ws.current.readyState !== WebSocket.OPEN ||
-        blockInfoSend
+        !allowInfoSend
       ) {
         return;
       }
@@ -67,11 +54,11 @@ export default function CoCursorProvider({
         x: e.pageX,
         y: e.pageY,
         visible: true,
-        name: userName.current,
+        name: myName || "anonymous",
       };
       ws.current?.send(JSON.stringify(cursorData));
     },
-    [blockInfoSend]
+    [allowInfoSend, myName]
   );
   const throttleMS = (() => {
     if (quality === "high") return 0;
@@ -93,8 +80,42 @@ export default function CoCursorProvider({
     ws.current?.send(JSON.stringify(cursorData));
   };
 
+  // 웹소켓 연결 및 설정
+  useEffect(() => {
+    if (disabled) {
+      sendCursorOff();
+      ws.current?.close();
+      ws.current = null;
+      return;
+    }
+
+    let wsURL = `${import.meta.env.VITE_COCURSOR_SERVER_URL}`;
+    if (channel) {
+      wsURL = `${import.meta.env.VITE_COCURSOR_SERVER_URL}?channel=${channel}`;
+    }
+
+    // 웹소켓 연결
+    ws.current = new WebSocket(wsURL, [import.meta.env.VITE_COCURSOR_API_KEY]);
+
+    // 웹소켓 메시지 수신
+    ws.current.onmessage = (event) => {
+      const cursorData: CursorData = JSON.parse(event.data);
+      setCursors((prev) => ({ ...prev, [cursorData.id]: cursorData }));
+    };
+
+    return () => {
+      // 웹소켓 연결 종료
+      sendCursorOff();
+      ws.current?.close();
+    };
+  }, [channel, disabled]);
+
   // 마우스 움직임에 따른 메시지 송신
   useEffect(() => {
+    if (disabled) {
+      return;
+    }
+
     window.addEventListener("mousemove", throttledSendCursorPosition);
     document.addEventListener("mouseleave", sendCursorOff);
 
@@ -102,21 +123,35 @@ export default function CoCursorProvider({
       window.removeEventListener("mousemove", throttledSendCursorPosition);
       document.removeEventListener("mouseleave", sendCursorOff);
     };
-  }, [throttledSendCursorPosition]);
+  }, [throttledSendCursorPosition, disabled]);
 
   // 정보 송신 거부 시 커서 삭제
   useEffect(() => {
-    if (blockInfoSend) {
+    if (!allowInfoSend) {
       sendCursorOff();
     }
-  }, [blockInfoSend]);
+  }, [allowInfoSend]);
 
   return (
-    <>
-      {Object.values(cursors).map((cursorData) => (
-        <Cursor key={cursorData.id} data={cursorData} />
-      ))}
+    <CoCursorContext.Provider
+      value={{
+        channel,
+        setChannel,
+        myName,
+        setMyName,
+        allowInfoSend,
+        setAllowInfoSend,
+        quality,
+        setQuality,
+        disabled,
+        setDisabled,
+      }}
+    >
+      {!disabled &&
+        Object.values(cursors).map((cursorData) => (
+          <Cursor key={cursorData.id} data={cursorData} />
+        ))}
       {children}
-    </>
+    </CoCursorContext.Provider>
   );
 }
